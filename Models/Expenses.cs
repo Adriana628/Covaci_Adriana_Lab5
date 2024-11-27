3f50aeae658ef87edc042919f3aab3f63f715a7c
Adriana Covaci <adrianacovaci22@gmail.com>
2024-11-27 14:15:55 +0200

    Add project files.

---
+++ Models/Expenses.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Covaci_Adriana_Lab5.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Secret { get; set; }
+
+    }
+}
